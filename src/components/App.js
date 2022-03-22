61be07b39564a96bed401acdd07897759910d615
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-03-22 01:47:20 -0300

    Commit Inicial
    -estructura inicial de react
    -se agrega los componentes basicos
    -se agrega favicon

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+
+import Navbar from './Navbar';
+import Hero from './Hero';
+import About from './About';
+import Skills from './Skills';
+import Experience from './Experience';
+import Contact from './Contact';
+import Footer from './Footer';
+
+import './styles/App.css';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Experience />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './styles/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <>
+      <a href="#" className='logo'>
+        <img src="" alt=""  className='logo-img'/>
+      </a>
+
+      <nav>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </nav>
+
+
+      <a href="" className='skin'></a>
+    </>
+  )
+}
+
+export default Navbar
