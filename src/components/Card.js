0f72f656fc738384ce5781ef153088a8e464ded2
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-05-17 06:26:29 -0300

    Funcionalidad completa de la ventana modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,37 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import PropTypes from 'prop-types';
+
+
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 import './styles/Card.css';
 import { DataContext } from '../Helpers/Context';
 
-const Card = ({imgUrl, title , desc, gitUrl, demoUrl}) => {
+
+
+const Card = ({imgUrl, title , desc, gitUrl, demoUrl, techList}) => {
+
+  const style = {
+    display : 'flex',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '600',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
   const {contextSkin} = useContext(DataContext)
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {setOpen(true)};
+  const handleClose = () => {setOpen(false)};
+
 
   const HandlerGit = () =>{
     window.open(`${gitUrl}`, '_blank')
@@ -16,20 +41,57 @@ const Card = ({imgUrl, title , desc, gitUrl, demoUrl}) => {
     window.open(`${demoUrl}`, '_blank')
   }
 
-  const handleModal = () => {
-    alert('modal')
-  }
+
 
   return (
-    <div className={contextSkin? 'card card-dark' : 'card'} onClick={handleModal}>
-        <img src={imgUrl} alt="" />
-        <h2>{title}</h2>
-        <p>{desc}</p>
-        <div className={contextSkin? 'buttons-card buttons-card-dark' : 'buttons-card'}>
-          <button onClick={HandlerDemo}>view project</button>
-          <button onClick={HandlerGit}>view github repo</button>
-        </div>
-    </div>
+    <>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} className={contextSkin ? 'modal-dark' : 'modal'}>
+          <div className={contextSkin ? 'modalImg modalImg-dark' : 'modalImg'}>
+            <img src={imgUrl} alt="" />
+          </div>
+
+          <div className={contextSkin ? 'modalBody modalBody-dark' : 'modalBody'}>
+            <Typography 
+              className={contextSkin ? 'modalTitle modalTitle-dark' : 'modalTitle'}
+              id="modal-modal-title" 
+              variant="h6" 
+              component="h2">
+              {title}
+            </Typography>
+            <Typography 
+              className={contextSkin ? 'modalDesc-dark modalDesc' : 'modalDesc'  }
+              id="modal-modal-description" 
+              sx={{ mt: 2, mb:2 }}>
+                {desc}
+            </Typography>
+            <p className={contextSkin ? 'modalDesc-dark modalDesc' : 'modalDesc'  }>Tecnologias aplicadas:</p>
+            <div className={contextSkin ? "modalTechs modalTechs-dark" : "modalTechs"}>
+              {techList.map((t) => {
+                return (
+                  <p className={contextSkin ? 'modalTech modalTech-dark' : 'modalTech'}>{t}</p>
+                )
+              })}
+            </div>
+          </div>
+        </Box>
+      </Modal>
+
+      <div className={contextSkin? 'card card-dark' : 'card'} onClick={handleOpen}>
+          <img src={imgUrl} alt="" />
+          <h2>{title}</h2>
+          <p>{desc}</p>
+          <div className={contextSkin? 'buttons-card buttons-card-dark' : 'buttons-card'}>
+            <button onClick={HandlerDemo}>View Demo</button>
+            <button onClick={HandlerGit}>View GitHub Repo</button>
+          </div>
+      </div>
+    </>
   )
 }
 
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,63 +11,84 @@ const Cards = ({isExp = true}) => {
   return (
     <div className='cards'>
         <div className="cards-exp" style={{display: isExp ? 'grid' : 'none' }}>
-          <Card 
-            imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-            title='Gestión de ventas Alpha'
-            desc='Registro y control de ventas, turnos, instructores y alumnos.'
-            gitUrl='https://github.com/FacundoSerna96/gestion-alfa-fitness'
-            demoUrl=''/>
-          <Card 
-            imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-            title='Quiz TKD'
-            desc='Juego de preguntas y respuestas para practicantes de Taekwondo'
-            gitUrl='https://github.com/FacundoSerna96/QuizTKD'
-            demoUrl=''/>
-          <Card 
-            imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-            title='Kiap Tournament'
-            desc='Sistema de puntaje para torneos de taekwondo y kickboxing'
-            gitUrl='https://github.com/FacundoSerna96/kiapTournament'
-            demoUrl=''/>
+          {isExp 
+            ? 
+            <>
+            <Card 
+              imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
+              title='Gestión de ventas Alpha'
+              desc='Registro y control de ventas, turnos, instructores y alumnos.'
+              gitUrl='https://github.com/FacundoSerna96/gestion-alfa-fitness'
+              demoUrl=''
+              techList={['Material UI', 'React Router', 'Axios', 'JWT', 'MongoDB']}/>
+            <Card 
+              imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
+              title='Quiz TKD'
+              desc='Juego de preguntas y respuestas para practicantes de Taekwondo'
+              gitUrl='https://github.com/FacundoSerna96/QuizTKD'
+              demoUrl='https://quiz-tkd.herokuapp.com/'
+              techList={['Vanilla Javascript', 'Bootstrap', 'JWT', 'MongoDB']}/>
+            <Card 
+              imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
+              title='Kiap Tournament'
+              desc='Sistema de puntaje para torneos de taekwondo y kickboxing'
+              gitUrl='https://github.com/FacundoSerna96/kiapTournament'
+              demoUrl=''
+              techList={['PHP', 'jQuery', 'Bootstrap', 'MySQL']}/>
+            </>
+            : 
+              ''}
         </div>
 
         <div className="cards-projects" style={{display: isExp ? 'none' : 'grid' }}>
-            <Card 
-              imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-              title='Social Media App'
-              desc='Clon de la red social Facebook'
-              gitUrl=''
-              demoUrl=''/>
-            <Card 
-              imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-              title='Panel de administracion'
-              desc='registro y control de ventas, turnos, instructores y alumnos.'
-              gitUrl=''
-              demoUrl=''/>
-            <Card 
-              imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-              title='Red Social UI'
-              desc='Red social usando la libreria de  Material UI'
-              gitUrl=''
-              demoUrl=''/>
+            {isExp 
+              ? 
+               ''
+              : 
+               <>
+               <Card 
+                imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
+                title='Social Media App'
+                desc='Clon de la red social Facebook (con chat)'
+                gitUrl='https://github.com/FacundoSerna96/React-Social-Media-App'
+                demoUrl=''
+                techList={['React.js', 'Socket.io', 'Axios', 'Context API', 'MongoDB']}/>
               <Card 
-              imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-              title='CRUD with Node js'
-              desc='Simple CRUD (Create, Read, Update and Delete) en Node js'
-              gitUrl=''
-              demoUrl=''/>
-            <Card 
-              imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-              title='Node Cafe'
-              desc='Web Service basado en el curso "Node: De cero a experto" de Fernando Herrera'
-              gitUrl=''
-              demoUrl=''/>
-            <Card 
-              imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-              title="Layout's Practice"
-              desc='Practicas de maquetacion basados en diseños en Adobe Xd'
-              gitUrl=''
-              demoUrl=''/>
+                imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
+                title='Panel de administracion'
+                desc='Dashboard basico con Materia UI'
+                gitUrl='https://github.com/FacundoSerna96/React-Admin-Panel'
+                demoUrl='https://facundoserna96.github.io/React-Admin-Panel/'
+                techList={['React.js', 'Material UI','React Router', 'Sass']}/>
+              <Card 
+                imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
+                title='Red Social UI'
+                desc='Red social usando la libreria de  Material UI'
+                gitUrl='https://github.com/FacundoSerna96/react-mui'
+                demoUrl='https://facundoserna96.github.io/react-mui/'
+                techList={['React.js','Material UI']}/>
+              <Card 
+                imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
+                title='CRUD with Node js'
+                desc='Simple CRUD (Create, Read, Update and Delete) en Node js'
+                gitUrl='https://github.com/FacundoSerna96/crud-with-node-js'
+                demoUrl=''
+                techList={['TypeScript', 'Bootstrap', 'jQuery', 'MVC', 'MySQL']}/>
+              <Card 
+                imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
+                title='Node Cafe'
+                desc='Web Service basado en el curso "Node: De cero a experto" de Fernando Herrera'
+                gitUrl='https://github.com/FacundoSerna96/NodeCafe'
+                demoUrl=''
+                techList={['Node JS','Express', 'MongoDB', 'Mongoose', 'JWT']}/>
+              <Card 
+                imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
+                title="Layout's Practice"
+                desc='Practicas de maquetacion basados en diseños en Adobe Xd'
+                gitUrl='https://github.com/FacundoSerna96/layoutPractice'
+                demoUrl='https://github.com/FacundoSerna96/layoutPractice'
+                techList={['HTML', 'CSS', 'Javascript', 'Adobe Xd']}/>
+               </>}
         </div>
     </div>
   )
