de69d8e43208a1fbf8e220bbaddb59c0ba145d08
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-05-17 00:46:03 -0300

    Se modifica el texto de algunas card

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import './styles/Card.css';
 import { DataContext } from '../Helpers/Context';
 
-const Card = ({imgUrl='', title='' , desc='', gitUrl='', demoUrl=''}) => {
+const Card = ({imgUrl, title , desc, gitUrl, demoUrl}) => {
 
   const {contextSkin} = useContext(DataContext)
 
@@ -16,14 +16,18 @@ const Card = ({imgUrl='', title='' , desc='', gitUrl='', demoUrl=''}) => {
     window.open(`${demoUrl}`, '_blank')
   }
 
+  const handleModal = () => {
+    alert('modal')
+  }
+
   return (
-    <div className={contextSkin? 'card card-dark' : 'card'}>
+    <div className={contextSkin? 'card card-dark' : 'card'} onClick={handleModal}>
         <img src={imgUrl} alt="" />
         <h2>{title}</h2>
         <p>{desc}</p>
         <div className={contextSkin? 'buttons-card buttons-card-dark' : 'buttons-card'}>
-          <button onClick={HandlerGit}>view project</button>
-          <button onClick={HandlerDemo}>view github repo</button>
+          <button onClick={HandlerDemo}>view project</button>
+          <button onClick={HandlerGit}>view github repo</button>
         </div>
     </div>
   )
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -14,58 +14,58 @@ const Cards = ({isExp = true}) => {
           <Card 
             imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
             title='Gestión de ventas Alpha'
-            desc='registro y control de ventas, turnos, instructores y alumnos.'
-            gitUrl=''
+            desc='Registro y control de ventas, turnos, instructores y alumnos.'
+            gitUrl='https://github.com/FacundoSerna96/gestion-alfa-fitness'
             demoUrl=''/>
           <Card 
             imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-            title='Gestión de ventas Alpha'
-            desc='registro y control de ventas, turnos, instructores y alumnos.'
-            gitUrl=''
+            title='Quiz TKD'
+            desc='Juego de preguntas y respuestas para practicantes de Taekwondo'
+            gitUrl='https://github.com/FacundoSerna96/QuizTKD'
             demoUrl=''/>
           <Card 
             imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-            title='Gestión de ventas Alpha'
-            desc='registro y control de ventas, turnos, instructores y alumnos.'
-            gitUrl=''
+            title='Kiap Tournament'
+            desc='Sistema de puntaje para torneos de taekwondo y kickboxing'
+            gitUrl='https://github.com/FacundoSerna96/kiapTournament'
             demoUrl=''/>
         </div>
 
         <div className="cards-projects" style={{display: isExp ? 'none' : 'grid' }}>
             <Card 
               imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-              title='Gestión de ventas Alpha'
+              title='Social Media App'
+              desc='Clon de la red social Facebook'
+              gitUrl=''
+              demoUrl=''/>
+            <Card 
+              imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
+              title='Panel de administracion'
               desc='registro y control de ventas, turnos, instructores y alumnos.'
               gitUrl=''
               demoUrl=''/>
             <Card 
               imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-              title='Gestión de ventas Alpha'
-              desc='registro y control de ventas, turnos, instructores y alumnos.'
-              gitUrl=''
-              demoUrl=''/>
-            <Card 
-              imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-              title='Gestión de ventas Alpha'
-              desc='registro y control de ventas, turnos, instructores y alumnos.'
+              title='Red Social UI'
+              desc='Red social usando la libreria de  Material UI'
               gitUrl=''
               demoUrl=''/>
               <Card 
               imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-              title='Gestión de ventas Alpha'
-              desc='registro y control de ventas, turnos, instructores y alumnos.'
+              title='CRUD with Node js'
+              desc='Simple CRUD (Create, Read, Update and Delete) en Node js'
               gitUrl=''
               demoUrl=''/>
             <Card 
               imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-              title='Gestión de ventas Alpha'
-              desc='registro y control de ventas, turnos, instructores y alumnos.'
+              title='Node Cafe'
+              desc='Web Service basado en el curso "Node: De cero a experto" de Fernando Herrera'
               gitUrl=''
               demoUrl=''/>
             <Card 
               imgUrl='https://themes.getbootstrap.com/wp-content/uploads/2019/06/1200x900-1-1200x900.png'
-              title='Gestión de ventas Alpha'
-              desc='registro y control de ventas, turnos, instructores y alumnos.'
+              title="Layout's Practice"
+              desc='Practicas de maquetacion basados en diseños en Adobe Xd'
               gitUrl=''
               demoUrl=''/>
         </div>
