cddb445d4096e695aa48613b5bca097bf21af48c
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2026-09-23 13:41:48 -0300

    Correcciones en los botones de descarga de cv en version en español

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ import profileCircle from "../assets/photos/profile-circle.jpg";
 
 import cv from "../assets/CV.pdf";
 import resume from "../assets/Resume.pdf";
+import cvFoto from "../assets/Cv con foto .pdf";
 
 import useOnScreen from "../Hooks/useOnScreen";
 import { DataContext } from "../Helpers/Context";
@@ -151,8 +152,26 @@ function About() {
             >
               <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zm-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0z" />
             </svg>
-            {language ? <p>Descargar CV</p> : <p>Download Resume</p>}
+            {language ? <p>CV (Formato Harvard)</p> : <p>Download Resume</p>}
           </button>
+
+          {language && (
+            <button
+              className="button-cv"
+              onClick={() => window.open(cvFoto, "_blank")}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="15"
+                height="15"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zm-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0z" />
+              </svg>
+              <p>CV con Foto</p>
+            </button>
+          )}
         </div>
       </div>
     </div>
